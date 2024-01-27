4429b0675493d728af0ac08f6477384e7fc1c0b6
ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>
2024-01-27 19:14:16 +0000

    feat: makes pulling & pushing of images config driven
    Signed-off-by: ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module replicator
 
 go 1.21.6
 
-require github.com/google/go-containerregistry v0.17.0
+require (
+	github.com/google/go-containerregistry v0.17.0
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
 	github.com/containerd/stargz-snapshotter/estargz v0.14.3 // indirect
@@ -10,13 +13,17 @@ require (
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/docker v24.0.0+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/sirupsen/logrus v1.9.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/vbatts/tar-split v0.11.3 // indirect
 	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,28 +6,114 @@ import (
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"gopkg.in/yaml.v3"
 )
 
+type Image struct {
+	Name     string `yaml:"name"`
+	Url      string `yaml:"url"`
+	Tag      string `yaml:"tag"`
+	Digest   string `yaml:"digest"`
+	Cosigned struct {
+		Enabled   string `yaml:"enabled"`
+		Signature string `yaml:"signature"`
+	}
+	PushLocation struct {
+		Registry string `yaml:"registry"`
+		Path     string `yaml:"path"`
+	} `yaml:"push-location"`
+}
+
+type Registry struct {
+	Name string `yaml:"name"`
+	Url  string `yaml:"url"`
+}
+
+// correctly populate the data.
+type T struct {
+	Images     []Image    `yaml:"images"`
+	Registries []Registry `yaml:"registries"`
+}
+
+func buildImageUrl(imageUrl string, imageTag string, imageDigest string) string {
+	if imageDigest == "" {
+		return fmt.Sprintf("%s:%s", imageUrl, imageTag)
+	}
+	return fmt.Sprintf("%s:%s@%s", imageUrl, imageTag, imageDigest)
+}
+
 func main() {
-	ref, err := name.ParseReference("gcr.io/google-containers/pause")
-	if err != nil {
-		panic(err)
-	}
+	t := readFile()
 
-	fmt.Println("Downloading...")
-	img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Downloaded")
+	for _, image := range t.Images {
+		imageUrl := buildImageUrl(image.Url, image.Tag, image.Digest)
+		ref, err := name.ParseReference(imageUrl)
+		check(err)
 
-	fp, err := os.CreateTemp(".", "img")
+		fmt.Printf("Downloading image: %s... \n", image.Name)
+		img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+		check(err)
+		fmt.Printf("Downloaded image: %s \n", image.Name)
+		// fmt.Printf("image details: %v \n", img)
+
+		hash, err := img.Digest()
+		check(err)
+
+		if image.Digest == hash.String() {
+			fmt.Println("digest verified")
+		}
+
+		newPlace := getRegistryUrl(t, image)
+		newUrl := fmt.Sprintf("%s/%s:%s", newPlace, image.PushLocation.Path, image.Tag)
+		tag, err := name.ParseReference(newUrl)
+		check(err)
+
+		fmt.Printf("Pushing image: %s ... \n", newUrl)
+
+		remote.Write(tag, img, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+		fmt.Printf("Pushed image: %s \n", newUrl)
+	}
+}
+
+func getRegistryUrl(t T, image Image) string {
+	for _, r := range t.Registries {
+		if r.Name == image.PushLocation.Registry {
+			return r.Url
+		}
+	}
+	panic(fmt.Errorf("no registries found for image push registry"))
+}
+
+func readFile() T {
+	dat, err := os.ReadFile("./config.yaml")
+	check(err)
+	t := T{}
+	err = yaml.Unmarshal(dat, &t)
+	check(err)
+	return t
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// unused for now
+func createFile(image Image, img v1.Image) {
+	fp, err := os.CreateTemp(".", fmt.Sprintf("%s-%s", image.Name, image.Tag))
+	check(err)
+
+	newTag, err := name.NewTag(fmt.Sprintf("%s:%s", image.Name, image.Tag))
+	check(err)
+
 	defer fp.Close()
 	fmt.Println("Writing to tarball...")
-	if err := tarball.WriteToFile(fp.Name(), ref, img); err != nil {
-		fmt.Errorf("Error writing image to tarball: %v", err)
+	if err := tarball.Write(newTag, img, fp); err != nil {
+		panic(fmt.Errorf("error writing image to tarball: %v", err))
 	}
 	fmt.Println("Written")
 }
